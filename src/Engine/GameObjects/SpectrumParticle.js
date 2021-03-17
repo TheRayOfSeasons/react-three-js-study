858aecbe325cc7cb9b4b10fddc889d4ad4b00d5c
TheRayofSeasons <raylawrencehenrisison@gmail.com>
2021-03-17 22:11:36 +0800

    Reference: Galaxy

--- src/Engine/GameObjects/SpectrumParticle.js
+++ src/Engine/GameObjects/SpectrumParticle.js
@@ -1,4 +1,4 @@
-import { AdditiveBlending, BufferAttribute, BufferGeometry, Clock, Group, Points, PointsMaterial, SphereBufferGeometry } from 'three';
+import { AdditiveBlending, BufferAttribute, BufferGeometry, Clock, Color, Group, Points, PointsMaterial, SphereBufferGeometry } from 'three';
 import { MonoBehaviour, GameObject } from '../Core/Behaviour';
 
 const clock = new Clock();
@@ -88,13 +88,16 @@ class BufferedParticles extends MonoBehaviour {
 
 class GalaxyParticle extends MonoBehaviour {
   parameters = {
-    count: 1000,
+    count: 100000,
     size: 0.02,
     radius: 5,
     branches: 3,
-    spin: 0.5,
+    spin: 1,
     speed: 2,
-    randomness: 0.1,
+    randomness: 0.2,
+    spread: 5,
+    innerColor: 0xff3c30,
+    outerColor: 0x1b7184,
   }
 
   start() {
@@ -102,29 +105,49 @@ class GalaxyParticle extends MonoBehaviour {
     const generateGalaxy = () => {
       const geometry = new BufferGeometry();
       const positions = new Float32Array(this.parameters.count * 3);
+      const colors = new Float32Array(this.parameters.count * 3);
+
+      const colorInside = new Color(this.parameters.innerColor);
+      const colorOutside = new Color(this.parameters.outerColor);
+
       for(let i = 0; i < this.parameters.count; i++) {
         const i3 = i * 3;
         const x = i3;
         const y = i3 + 1;
         const z = i3 + 2;
+
+        // Positions
         const radius = Math.random() * this.parameters.radius;
         const spinAngle = radius * this.parameters.spin;
         const branchAngle = (i % this.parameters.branches) / this.parameters.branches * Math.PI * 2;
 
-        const randomX = (Math.random() - 0.5) * this.parameters.randomness;
-        const randomY = (Math.random() - 0.5) * this.parameters.randomness;
-        const randomZ = (Math.random() - 0.5) * this.parameters.randomness;
+        // const randomX = (Math.random() - 0.5) * this.parameters.randomness;
+        // const randomY = (Math.random() - 0.5) * this.parameters.randomness;
+        // const randomZ = (Math.random() - 0.5) * this.parameters.randomness;
+
+        const randomX = Math.pow(Math.random(), this.parameters.spread) * (Math.random() < 0.5 ? 1 : -1);
+        const randomY = Math.pow(Math.random(), this.parameters.spread) * (Math.random() < 0.5 ? 1 : -1);
+        const randomZ = Math.pow(Math.random(), this.parameters.spread) * (Math.random() < 0.5 ? 1 : -1);
 
         positions[x] = Math.cos(branchAngle + spinAngle) * radius + randomX;
         positions[y] = randomY;
         positions[z] = Math.sin(branchAngle + spinAngle) * radius + randomZ;
+
+        // Colors
+        const mixedColor = colorInside.clone();
+        mixedColor.lerp(colorOutside, radius / this.parameters.radius);
+        colors[x] = mixedColor.r;
+        colors[y] = mixedColor.g;
+        colors[z] = mixedColor.b;
       }
       geometry.setAttribute('position', new BufferAttribute(positions, 3));
+      geometry.setAttribute('color', new BufferAttribute(colors, 3));
       const material = new PointsMaterial({
         size: this.parameters.size,
         sizeAttenuation: true,
         depthWrite: false,
-        blending: AdditiveBlending
+        blending: AdditiveBlending,
+        vertexColors: true
       });
       const points = new Points(geometry, material);
       this.geometry = geometry;
@@ -136,7 +159,7 @@ class GalaxyParticle extends MonoBehaviour {
 
   update(time) {
     const elapsedTime = clock.getElapsedTime();
-    this.points.rotation.y = elapsedTime * this.parameters.speed;
+    // this.points.rotation.y = elapsedTime * this.parameters.speed;
   }
 
   exportAsSceneObject() {
