6911b734b0e80c2d9e6970cfe8bcbd181d5bc24b
TheRayofSeasons <raylawrencehenrisison@gmail.com>
2021-03-21 10:46:05 +0800

    Modify: Wave movement for spectrum

--- src/Engine/GameObjects/Spectrum.js
+++ src/Engine/GameObjects/Spectrum.js
@@ -7,23 +7,34 @@ const colors = [0xf7a541, 0xf45d4c, 0xfa2e59, 0x4783c3, 0x9c6cb7];
 
 class Particle extends MonoBehaviour {
   parameters = {
-    count: 100
+    count: 1000,
+    waveIntensity: 2
   };
   particles = [];
 
   start() {
     this.group = new Group();
     const geometryCore = new BoxGeometry(0.035, 0.035, 0.035);
-    
+    const particlesGeometry = new BufferGeometry();
+    const particlesMaterial = new PointsMaterial({
+      size: 0.02,
+      sizeAttenuation: true,
+      depthWrite: false,
+      blending: AdditiveBlending,
+      vertexColors: true,
+      transparent: true,
+    });
+
     const positions = new Float32Array(this.parameters.count * 3);
+    const colors = new Float32Array(this.parameters.count * 3);
     for(let i = 0, i3 = 0; i < this.parameters.count; i++, i3 += 3) {
       const x = i3;
       const y = i3 + 1;
       const z = i3 + 2;
       positions[x] = (Math.random() - 0.5) * 2;
-      positions[y] = 0;
+      positions[y] = (Math.random() - 0.5) * 2;
       positions[z] = (Math.random() - 0.5) * 2;
-      
+
       const particle = new Object3D();
       const materialCore = new MeshNormalMaterial({
         color: colors[i % colors.length],
@@ -39,17 +50,29 @@ class Particle extends MonoBehaviour {
       this.group.add(particle);
       this.geometryCore = geometryCore;
     }
-    console.log(this.particles);
+    particlesGeometry.setAttribute('position', new BufferAttribute(positions, 3));
+    particlesGeometry.setAttribute('color', new BufferAttribute(colors, 3));
+    const particles = new Points(particlesGeometry, particlesMaterial);
+    this.group.add(particles);
+    this.particlesGeometry = particlesGeometry;
   }
 
   update(time) {
     const elapsedTime = clock.getElapsedTime();
-    // console.log(this.group.children[10].children[0].position);
     for(let i = 0; i < this.parameters.count; i++) {
-      const xValue = this.particles[i].position.x;
-      this.particles[i].position.y = Math.sin(elapsedTime + xValue);
+      let i3 = i * 3;
+      let x = i3;
+      let y = i3 + 1;
+      let z = i3 + 2;
+      const xValue = this.particlesGeometry.attributes.position.array[x];
+      this.particlesGeometry.attributes.position.array[y] = Math.sin(elapsedTime + xValue * this.parameters.waveIntensity);
+
+      this.particles[i].position.x = this.particlesGeometry.attributes.position.array[x];
+      this.particles[i].position.y = this.particlesGeometry.attributes.position.array[y];
+      this.particles[i].position.z = this.particlesGeometry.attributes.position.array[z];
       this.particles[i].position.needsUpdate = true;
     }
+    this.particlesGeometry.attributes.position.needsUpdate = true;
   }
 
   exportAsSceneObject() {
